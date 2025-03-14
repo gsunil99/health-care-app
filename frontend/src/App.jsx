63d6665b675930773b7fc12f8246a71ce7820bb7
Sunil G <gsunil99910@gmail.com>
2025-03-14 16:34:13 +0530

    added react toaster for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,11 @@ import Myappointments from './pages/Myappointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-
+import { ToastContainer } from 'react-toastify';
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer/>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
