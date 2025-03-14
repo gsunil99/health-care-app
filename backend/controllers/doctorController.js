08f3e5aa1776d83dedf709deda57cabfbfb3e4ea
Sunil G <gsunil99910@gmail.com>
2025-03-14 16:33:04 +0530

    implemented get all doctors api

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -10,3 +10,12 @@ export const changeAvailabitly = async(req,res) =>{
         res.json({success:false,message:error.message})
     }
 }
+export const doctorList = async(req,res) =>{
+    try {
+        const doctors = await doctorModel.find({}).select(['-password','-email'])
+        res.json({success:true,doctors})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
+}
