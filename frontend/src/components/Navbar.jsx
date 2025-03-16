70e9ea6f3b40338711e120426af56f06aedb4f0c
Sunil G <gsunil99910@gmail.com>
2025-03-16 15:27:41 +0530

    navbar updated for user data

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets';
 import { NavLink, useNavigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
 
 const Navbar = () => {
     const navigate = useNavigate();
     const [showMenu, setShowMenu] = useState(false);
-    const [token, setToken] = useState(true);
+    const {token,setToken,userData} = useContext(AppContext);
+    const logOut = ()=>{
+        setToken(false);
+        localStorage.removeItem('token');
+    }
     return (
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
             <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt=''></img>
@@ -29,14 +34,14 @@ const Navbar = () => {
             </ul>
             <div className='flex items-center gap-4'>
                 {
-                    token ? <div className='flex items-center gap-2 cursor-pointer group relative'>
-                        <img className='w-8 rounded-full' src={assets.profile_pic}></img>
+                    token && userData ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                        <img className='w-8 rounded-full' src={userData.image}></img>
                         <img className='w-2.5' src={assets.dropdown_icon}></img>
                         <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                             <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                                 <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                                 <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                                <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                                <p onClick={logOut} className='hover:text-black cursor-pointer'>Logout</p>
                             </div>
                         </div>
                     </div>
