819b4b04fcff093896d5062fd62bf84ff5a72dda
Sunil G <gsunil99910@gmail.com>
2025-03-17 23:14:27 +0530

    added appointment model

---
+++ backend/models/appointmentModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const appointmentSchema = new mongoose.Schema({
+    userId:{type:String,required:true},
+    docId:{type:String,required:true},
+    slotDate:{type:String,required:true},
+    slotTime:{type:String,required:true},
+    userData:{type:Object,required:true},
+    docData:{type:String,required:true},
+    amount:{type:String,required:true},
+    date:{type:String,required:true},
+    cancelled:{type:Boolean,default:false},
+    payment:{type:Boolean,default:false},
+    isCompleted:{type:Boolean,default:false},
+})
+
+export const appointmentModel = mongoose.models.appointment || mongoose.model('appointment',appointmentSchema)
