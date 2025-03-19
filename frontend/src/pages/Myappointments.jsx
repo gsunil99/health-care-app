892d086f4c0b77c3df707ef848dac5b18d58bcb0
Sunil G <gsunil99910@gmail.com>
2025-03-19 23:47:51 +0530

    cancel appointment is integrated with FE

--- frontend/src/pages/Myappointments.jsx
+++ frontend/src/pages/Myappointments.jsx
@@ -7,11 +7,34 @@ const Myappointments = () => {
   const { backendUrl,token } = useContext(AppContext)
   const [appointments,setAppointments] = useState([]);
 
+  const months = ["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
+  const slotDateFormat = (slotDate)=>{
+    const dateArray = slotDate.split('_')
+    return dateArray[0]+ " " + months[Number(dateArray[1])]+" "+dateArray[2]
+  }
   const getUsersAppointment = async()=>{
     try {
       const {data} = await axios.get(backendUrl+'/api/user/appointments',{headers:{token}})
       if(data.success){
         setAppointments(data.appointments.reverse())
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message);
+    }
+  }
+
+  const cancelAppointment = async(appointmentId)=>{
+    console.log(appointmentId);
+    try {
+      const {data} = await axios.post(backendUrl + '/api/user/cancel-appointment',{appointmentId},{headers:{token}})
+      if(data.success){
+        toast.success(data.message);
+        getUsersAppointment();
+      }
+      else{
+        toast.error(data.message);
       }
     } catch (error) {
       console.log(error)
@@ -38,12 +61,13 @@ const Myappointments = () => {
               <p className='text-zinc-700 font-medium mt-1'>Address: </p>
               <p className='text-xs'>{item.docData?.address?.line1}</p>
               <p className='text-xs'>{item.docData?.address?.line2}</p>
-              <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span>{item?.slotDate} | {item?.slotTime} </p>
+              <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {slotDateFormat(item?.slotDate)} | {item?.slotTime} </p>
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-center'>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+              {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+              {!item.cancelled && <button onClick={()=>{cancelAppointment(item._id)}} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+              {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment cancelled</button>}
             </div>
           </div>
         ))}
