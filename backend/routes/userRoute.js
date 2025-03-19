6274903dbacecf4024444c53ce7c12567d3cd180
Sunil G <gsunil99910@gmail.com>
2025-03-19 23:47:08 +0530

    added route for cancel appointment

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { bookAppointment, getAllAppointments, getUserDetails, loginUser, registerUser, updateUserDetails } from '../controllers/userController.js'
+import { bookAppointment, cancelAppointment, getAllAppointments, getUserDetails, loginUser, registerUser, updateUserDetails } from '../controllers/userController.js'
 import { authUser } from '../middlewares/auth-user.js'
 import upload from '../middlewares/multer.js'
 const userRouter = express.Router()
@@ -9,4 +9,5 @@ userRouter.get('/get-profile',authUser,getUserDetails)
 userRouter.post('/update-profile',upload.single('image'),authUser,updateUserDetails)
 userRouter.post('/book-appointment',authUser,bookAppointment)
 userRouter.get('/appointments',authUser,getAllAppointments)
+userRouter.post('/cancel-appointment',authUser,cancelAppointment)
 export default userRouter
