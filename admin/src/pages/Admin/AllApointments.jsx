72ef75dcd1d583303a78f1893e4f157e13186340
Sunil G <gsunil99910@gmail.com>
2025-03-21 00:25:13 +0530

    all apointments component updated

--- admin/src/pages/Admin/AllApointments.jsx
+++ admin/src/pages/Admin/AllApointments.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../../context/AdminContext.jsx'
+import { useEffect } from 'react';
 
 const AllApointments = () => {
+  const {aToken,appointments,getAllAppointments} = useContext(AdminContext);
+  useEffect(()=>{
+    if(aToken){
+      getAllAppointments();
+    }
+  },[aToken])
   return (
     <div>
-      
+      <p>All appointments</p>
+      <div>
+        <div>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Actions</p>
+        </div>
+      </div>
     </div>
   )
 }
