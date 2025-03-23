bc35537b4eadd465ec662579fc0e2e09ea75bcfa
Sunil G <gsunil99910@gmail.com>
2025-03-23 12:32:46 +0530

    all appointments page is updated for admin

--- admin/src/pages/Admin/AllApointments.jsx
+++ admin/src/pages/Admin/AllApointments.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import { useContext } from 'react'
 import { AdminContext } from '../../context/AdminContext.jsx'
 import { useEffect } from 'react';
+import { AppContext } from '../../context/Appcontext.jsx';
 
 const AllApointments = () => {
   const {aToken,appointments,getAllAppointments} = useContext(AdminContext);
+  const {calculateAge,slotDateFormat} = useContext(AppContext)
   useEffect(()=>{
     if(aToken){
       getAllAppointments();
     }
   },[aToken])
+
+
   return (
-    <div>
-      <p>All appointments</p>
-      <div>
-        <div>
+    <div className='w-full max-w-6xl m-5'>
+      <p className='mb-3 text-lg font-medium'>All appointments</p>
+      <div className='bg-white border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll'>
+        <div className='hidden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b'>
           <p>#</p>
           <p>Patient</p>
           <p>Age</p>
@@ -23,6 +27,22 @@ const AllApointments = () => {
           <p>Fees</p>
           <p>Actions</p>
         </div>
+        {appointments?.map((item,index)=>(
+          <div className='flex flex-wrap justify-between max-sm:gap-2 sm:grid sm:grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50' key={index}>
+            <p>{index + 1}</p>
+            <div className='flex items-center gap-2'>
+              <img className='w-8 rounded-full' src={item.userData.image} alt=''/>
+              <p>{item.userData.name}</p>
+            </div>
+            <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
+            <p>{slotDateFormat(item.slotDate)}, {item.slotTime}</p>
+            <div className='flex items-center gap-2'>
+              <img className='w-8 rounded-full bg-gray-200' src={item.docData.image} alt=''/>
+              <p>{item.docData.name}</p>
+            </div>
+            <p></p>
+          </div>
+        ))}
       </div>
     </div>
   )
