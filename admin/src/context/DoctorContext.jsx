61cf335b82a06544bd0584ee35447d6180e8eb27
Sunil G <gsunil99910@gmail.com>
2025-03-24 14:10:32 +0530

    added appointments api in context

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -1,10 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { createContext } from "react";
-
+import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props)=>{
-    const value = {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [dToken,setDToken] = useState(localStorage.getItem('dToken')? localStorage.getItem('dToken'):'');
+    const [appointments,setAppointments] = useState([])
+    const getAppointments = async()=>{
+        try {
+            const {data} = await axios.get(backendUrl+'/api/doctor/appointments',{headers:{dToken}})
+            if(data.success){
+                setAppointments(data.appointments.reverse());
+                
+            }else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log('abcde ',error)
+            toast.error(error.message);
+        }
+    }
 
+    const value = {
+        dToken,setDToken,
+        backendUrl,
+        getAppointments,
+        appointments
     }
     return (
         <DoctorContext.Provider value={value}>
