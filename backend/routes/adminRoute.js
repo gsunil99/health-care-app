23c89b6eebea63acca47e286c6db3f97b21d218b
Sunil G <gsunil99910@gmail.com>
2025-03-24 14:16:10 +0530

    added new routes for admin

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addDoctor, allDoctors, appointmentsAdmin, loginAdmin } from '../controllers/adminController.js';
+import { addDoctor, adminDashboard, allDoctors, appointmentsAdmin, cancelAppointment, loginAdmin } from '../controllers/adminController.js';
 import upload from '../middlewares/multer.js';
 import authAdmin from '../middlewares/auth-admin.js';
 import { changeAvailabitly } from '../controllers/doctorController.js';
@@ -10,5 +10,6 @@ adminRouter.post('/login',loginAdmin);
 adminRouter.get('/all-doctors',authAdmin,allDoctors);
 adminRouter.post('/change-availabilty',authAdmin,changeAvailabitly);
 adminRouter.get('/appointments',authAdmin,appointmentsAdmin);
-
+adminRouter.post('/cancel-appointment',authAdmin,cancelAppointment);
+adminRouter.get('/dashboard',authAdmin,adminDashboard);
 export default adminRouter;
