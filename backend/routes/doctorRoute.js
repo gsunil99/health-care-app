16180a7e90aea2a613577765a729bd6270235a1b
Sunil G <gsunil99910@gmail.com>
2025-03-24 14:16:21 +0530

    added new routes for doctor

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,8 @@
 import express from 'express'
-import { doctorList } from '../controllers/doctorController.js';
+import { appointmentsDoctor, doctorList, loginDoctor } from '../controllers/doctorController.js';
+import authDoctor from '../middlewares/auth-doctor.js';
 
 export const doctorRouter = express.Router();
 doctorRouter.get('/list',doctorList)
+doctorRouter.post('/login',loginDoctor)
+doctorRouter.get('/appointments',authDoctor,appointmentsDoctor)
