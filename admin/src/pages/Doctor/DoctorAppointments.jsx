92adb282f5d2e91b297218e63e4a3e7d327fb473
Sunil G <gsunil99910@gmail.com>
2025-03-24 14:13:58 +0530

    doctor appointments page updated

---
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useContext } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
+import { useEffect } from 'react'
+import { AppContext } from '../../context/Appcontext'
+import { assets } from '../../assets/assets'
+
+const DoctorAppointments = () => {
+    const { dToken,
+        getAppointments,
+        appointments } = useContext(DoctorContext)
+
+    const { calculateAge,slotDateFormat,currency } = useContext(AppContext)
+
+    useEffect(() => {
+        if (dToken) {
+            getAppointments()
+        }
+    }, [dToken])
+    return (
+        <div className='w-full max-w-6xl m-5'>
+            <p className='mb-3 text-lg font-medium'>All Appointments</p>
+            <div className='bg-white border rounded text-sm max-h-[80vh] min-h-[50vh] overflow-y-scroll'>
+                <div className='max-sm:hidden grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 py-3 px-6 border-b'>
+                    <p>#</p>
+                    <p>Patient</p>
+                    <p>Payment</p>
+                    <p>Age</p>
+                    <p>Date & time</p>
+                    <p>Fees</p>
+                    <p>Action</p>
+                </div>
+                {appointments.map((item, index) => (
+                    <div className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b' key={index}>
+                        <p>
+                            {index + 1}
+                        </p>
+                        <div>
+                            <img className='' src={item.userData.image} alt="" /> <p>{item.userData.name}</p>
+                        </div>
+                        <div>
+                            <p>
+                                {item.payment ? 'Online' : 'Cash'}
+                            </p>
+                        </div>
+                        <p>{calculateAge(item.userData.dob)}</p>
+                        <p>{slotDateFormat(item.slotDate)}, {item.slotTime}</p>
+                        <p>{currency} {item.amount}</p>
+                        <div>
+                            <img src={assets.cancel_icon} alt="" />
+                            <img src={assets.tick_icon} alt="" />
+                        </div>
+                    </div>
+                ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default DoctorAppointments
