fd7ec829274a26ad3a45698e4ff1b83361f11e56
Sunil G <gsunil99910@gmail.com>
2025-03-26 11:21:59 +0530

    Book appointment only if doctor is available

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -9,95 +9,95 @@ import axios from 'axios'
 
 const Appointment = () => {
   const { doctorId } = useParams();
-  const { doctors,currencySymbol,backendUrl,getDoctorsData,token,userData } = useContext(AppContext);
+  const { doctors, currencySymbol, backendUrl, getDoctorsData, token, userData } = useContext(AppContext);
   const navigate = useNavigate();
   const [doctorInfo, setDoctorInfo] = useState(null);
-  const [doctorSlots,setDoctorSlots] = useState([]);
-  const [slotIndex,setSlotIndex] = useState(0);
-  const [slotTime,setSlotTime] = useState('');
-  const daysOfWeek = ['SUN','MON','TUE','WED','THU','FRI','SAT']
+  const [doctorSlots, setDoctorSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState('');
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
   const fetchDoctorInfo = async () => {
     const doctorInfo = doctors.find((doctor) => doctor._id === doctorId)
     setDoctorInfo(doctorInfo);
   }
-  const getAvailableSlots = async()=>{
+  const getAvailableSlots = async () => {
     setDoctorSlots([]);
 
     //getting current date
     let today = new Date();
     let validDays = 0; // Track valid days
 
-    for(let i=0;validDays<7;i++){
+    for (let i = 0; validDays < 7; i++) {
       //getting date with index
       let currentDate = new Date(today);
-      currentDate.setDate(today.getDate()+i);
+      currentDate.setDate(today.getDate() + i);
 
       //setting end time of the date with index
       let endTime = new Date();
-      endTime.setDate(today.getDate()+i);
-      endTime.setHours(21,0,0,0);
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
 
       //setting hours 
-      if(today.getDate() === currentDate.getDate()){
+      if (today.getDate() === currentDate.getDate()) {
         //hours will ideally start from 10 am but changes for that particular day
-        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours()+1 : 10)
-        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30:0)
-      } else{
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
         currentDate.setHours(10)
         currentDate.setMinutes(0)
       }
       // Skip this day if currentDate already exceeds endTime
-    if (currentDate >= endTime) continue;
+      if (currentDate >= endTime) continue;
 
       let timeSlots = [];
-      while(currentDate < endTime){
-        let formattedTime = currentDate.toLocaleTimeString([],{hour:'2-digit',minute:'2-digit',hour12:true})
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })
         let day = currentDate.getDate()
-        let month = currentDate.getMonth()+1
+        let month = currentDate.getMonth() + 1
         let year = currentDate.getFullYear()
 
-        const slotDate = day+'_'+month+'_'+year
+        const slotDate = day + '_' + month + '_' + year
         const slotTime = formattedTime
-        
-        const isSlotAvailable = doctorInfo.slots_booked[slotDate] && doctorInfo.slots_booked[slotDate].includes(slotTime) ? false : true 
-        
-        if(isSlotAvailable){
+
+        const isSlotAvailable = doctorInfo.slots_booked[slotDate] && doctorInfo.slots_booked[slotDate].includes(slotTime) ? false : true
+
+        if (isSlotAvailable) {
           //add slot to array
           timeSlots.push({
             dateTime: new Date(currentDate),
             time: formattedTime
           })
         }
-       
+
         //increment current time by 30minutes
-        currentDate.setMinutes(currentDate.getMinutes()+30)
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
       }
-      setDoctorSlots(prev=> ([...prev,timeSlots]));
+      setDoctorSlots(prev => ([...prev, timeSlots]));
       validDays++;
     }
   }
 
-  const bookAppointment = async()=>{
-    if(!token){
+  const bookAppointment = async () => {
+    if (!token) {
       toast.warn('Login to book appointment')
       return navigate('/login')
     }
-    if(!userData.name || !userData.email || !userData.image || !userData.address.line1 || !userData.address.line2 || !userData.dob || !userData.gender || !userData.phone){
+    if (!userData.name || !userData.email || !userData.image || !userData.address.line1 || !userData.address.line2 || !userData.dob || !userData.gender || !userData.phone) {
       toast.warn('Before booking appointment update your profile details!!!')
       return navigate('/my-profile')
     }
     try {
       const date = doctorSlots[slotIndex][0].dateTime
       let day = date.getDate();
-      let month = date.getMonth()+1;
+      let month = date.getMonth() + 1;
       let year = date.getFullYear()
-      const slotDate = day+'_'+month+'_'+year
-      const {data} = await axios.post(backendUrl+'/api/user/book-appointment',{docId:doctorId,slotDate,slotTime},{headers:{token}})
-      if(data.success){
+      const slotDate = day + '_' + month + '_' + year
+      const { data } = await axios.post(backendUrl + '/api/user/book-appointment', { docId: doctorId, slotDate, slotTime }, { headers: { token } })
+      if (data.success) {
         toast.success(data.message);
         getDoctorsData();
         navigate('/my-appointments')
-      }else{
+      } else {
         toast.error(data.message)
       }
     } catch (error) {
@@ -110,9 +110,9 @@ const Appointment = () => {
     fetchDoctorInfo();
   }, [doctorId, doctors])
 
-  useEffect(()=>{
+  useEffect(() => {
     getAvailableSlots();
-  },[doctorInfo])
+  }, [doctorInfo])
   return doctorInfo && (
     <div>
       {/* Doctor details */}
@@ -142,10 +142,10 @@ const Appointment = () => {
         <p>Booking Slots</p>
         <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
           {
-            doctorSlots.length && doctorSlots.map((item,index)=>(
-              <div onClick={()=>{
+            doctorSlots.length && doctorSlots.map((item, index) => (
+              <div onClick={() => {
                 setSlotIndex(index);
-              }} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white': 'border border-gray-200'}`} key={index}>
+              }} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} key={index}>
                 <p>{item[0] && daysOfWeek[item[0].dateTime.getDay()]}</p>
                 <p>{item[0] && item[0].dateTime.getDate()}</p>
               </div>
@@ -154,19 +154,29 @@ const Appointment = () => {
         </div>
         <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
           {
-            doctorSlots.length && doctorSlots[slotIndex].map((item,index)=>(
-              <p onClick={()=>{
+            doctorSlots.length && doctorSlots[slotIndex].map((item, index) => (
+              <p onClick={() => {
                 setSlotTime(item.time)
-              }} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white':'text-gray-400 border border-gray-300'}`} key={index}>
+              }} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
                 {item.time.toLowerCase()}
               </p>
             ))
           }
         </div>
-        <button onClick={bookAppointment} className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an appointment</button>
+        {doctorInfo.available ? (
+          <button
+            onClick={bookAppointment}
+            className="bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6"
+          >
+            Book an appointment
+          </button>
+        ) : (
+          <p className="text-red-500 text-sm font-medium my-6">Doctor not available for while</p>
+        )}
+
       </div>
       {/* listing related doctors */}
-      <RelatedDoctors doctorId={doctorInfo._id} speciality = {doctorInfo.speciality}/>
+      <RelatedDoctors doctorId={doctorInfo._id} speciality={doctorInfo.speciality} />
     </div>
   )
 }
