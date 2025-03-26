265f99d540b97bc111ffa3ad52e16b2607a96b9e
Sunil G <gsunil99910@gmail.com>
2025-03-26 11:20:35 +0530

    doctor availabilty made dynamic

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -2,17 +2,17 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom';
 
-const RelatedDoctors = ({doctorId,speciality}) => {
-    const {doctors} = useContext(AppContext);
-    const [relatedDoctors,setRelatedDoctors] = useState([]);
-    const navigate = useNavigate();
+const RelatedDoctors = ({ doctorId, speciality }) => {
+  const { doctors } = useContext(AppContext);
+  const [relatedDoctors, setRelatedDoctors] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        if(doctors && doctors.length > 0 && speciality){
-            const doctorsData = doctors.filter((doc)=>doc.speciality === speciality && doc._id !== doctorId);
-            setRelatedDoctors(doctorsData);
-        }
-    },[doctors,doctorId,speciality])
+  useEffect(() => {
+    if (doctors && doctors.length > 0 && speciality) {
+      const doctorsData = doctors.filter((doc) => doc.speciality === speciality && doc._id !== doctorId);
+      setRelatedDoctors(doctorsData);
+    }
+  }, [doctors, doctorId, speciality])
 
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-16'>
@@ -20,21 +20,21 @@ const RelatedDoctors = ({doctorId,speciality}) => {
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-6'>
         {
-            relatedDoctors.slice(0,5).map((ele,index)=>(
-                <div onClick={()=> {navigate(`/appointment/${ele._id}`);scrollTo(0,0)}} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
-                    <img src={ele.image} className='bg-blue-50'></img>
-                    <div className='p-4'>
-                        <div className='flex items-center gap-2 text-sm text-center text-green-500'>
-                            <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
-                        </div>
-                        <p className='text-gray-900 text-lg font-medium'>{ele.name}</p>
-                        <p className='text-gray-600 text-sm'>{ele.speciality}</p>
-                    </div>
+          relatedDoctors.slice(0, 5).map((ele, index) => (
+            <div onClick={() => { navigate(`/appointment/${ele._id}`); scrollTo(0, 0) }} key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+              <img src={ele.image} className='bg-blue-50'></img>
+              <div className='p-4'>
+                <div className={`flex items-center gap-2 text-sm text-center ${ele.available ? 'text-green-500' : 'text-gray-500'}`}>
+                  <p className={`w-2 h-2 ${ele.available ? 'bg-green-500' : 'bg-gray-500'} rounded-full`}></p>{ele.available ? <p>Available</p> : <p>Not Available</p>}
                 </div>
-            ))
+                <p className='text-gray-900 text-lg font-medium'>{ele.name}</p>
+                <p className='text-gray-600 text-sm'>{ele.speciality}</p>
+              </div>
+            </div>
+          ))
         }
       </div>
-      <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+      <button onClick={() => { navigate('/doctors'); scrollTo(0, 0) }} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
