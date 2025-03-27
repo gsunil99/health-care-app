ec33c1cde47717c1902dc628b7c21a1d0ca06cb5
Sunil G <gsunil99910@gmail.com>
2025-03-27 23:10:23 +0530

    token based navigation

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -5,19 +5,23 @@ import { useNavigate } from 'react-router-dom';
 import { DoctorContext } from '../context/DoctorContext';
 
 const Navbar = () => {
-    const {aToken,setAToken} = useContext(AdminContext);
-    const {dToken,setDToken} = useContext(DoctorContext)
-    const navigate = useNavigate();
-    const logOut = ()=>{
-        navigate('/')
-        aToken && setAToken('') && localStorage.removeItem('aToken');
-        dToken && setDToken('') && localStorage.removeItem('dToken');
-    }
+  const { aToken, setAToken } = useContext(AdminContext);
+  const { dToken, setDToken } = useContext(DoctorContext)
+  const navigate = useNavigate();
+  const logOut = () => {
+    navigate('/')
+    aToken && setAToken('') && localStorage.removeItem('aToken');
+    dToken && setDToken('') && localStorage.removeItem('dToken');
+  }
   return (
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
       <div className='flex items-center gap-2 text-xs'>
-        <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt=''/>
-        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin':'Doctor'}</p>
+        {/* <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt='' /> */}
+        <div className="flex items-center space-x-2 cursor-pointer" onClick={() => aToken ? navigate('/') : navigate('/doctor-dashboard')}>
+          <img className="w-12" src={assets.admin_logo} alt="Logo" />
+          <span className="text-xl md:text-3xl font-semibold text-gray-800">Care for Health</span>
+        </div>
+        <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
       </div>
       <button onClick={logOut} className='bg-primary text-white text-sm px-10 py-2 rounded-full'>LogOut</button>
     </div>
