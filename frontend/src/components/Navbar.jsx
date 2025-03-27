de527512cecddd1207155625c17bf1bf00653a75
Sunil G <gsunil99910@gmail.com>
2025-03-27 23:11:26 +0530

    logo updated for navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,14 +6,19 @@ import { AppContext } from '../context/AppContext';
 const Navbar = () => {
     const navigate = useNavigate();
     const [showMenu, setShowMenu] = useState(false);
-    const {token,setToken,userData} = useContext(AppContext);
-    const logOut = ()=>{
+    const { token, setToken, userData } = useContext(AppContext);
+    const logOut = () => {
         setToken(false);
         localStorage.removeItem('token');
     }
     return (
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-            <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt=''></img>
+            {/* <img onClick={()=>navigate('/')} className='w-16 cursor-pointer' src={assets.logo} alt=''></img> */}
+            <div className="flex items-center space-x-2 cursor-pointer" onClick={() => navigate('/')}>
+                <img className="w-12" src={assets.logo} alt="Logo" />
+                <span className="text-xl md:text-3xl font-semibold text-gray-800">Care for Health</span>
+            </div>
+
             <ul className='hidden md:flex items-start gap-5 font-medium'>
                 <NavLink to='/'>
                     <li className='py-1'>Home</li>
@@ -39,26 +44,29 @@ const Navbar = () => {
                         <img className='w-2.5' src={assets.dropdown_icon}></img>
                         <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                             <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                                <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                                <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                                <p onClick={() => navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                                <p onClick={() => navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
                                 <p onClick={logOut} className='hover:text-black cursor-pointer'>Logout</p>
                             </div>
                         </div>
                     </div>
                         : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
                 }
-                <img onClick={()=>{setShowMenu(true)}} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+                <img onClick={() => { setShowMenu(true) }} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
                 {/* Mobile Menu */}
-                <div className={`${showMenu ? 'fixed w-full': 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+                <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
                     <div className='flex items-center justify-between px-5 py-6'>
-                        <img className='w-36' src={assets.logo} alt="" />
-                        <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon}/>
+                        <div className="flex items-center space-x-2 cursor-pointer" onClick={() => navigate('/')}>
+                            <img className="w-8" src={assets.logo} alt="Logo" />
+                            <span className="text-xl md:text-3xl font-semibold text-gray-800">Care for Health</span>
+                        </div>
+                        <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} />
                     </div>
                     <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
-                        <NavLink to='/' onClick={()=>setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
-                        <NavLink to='/doctors' onClick={()=>setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>All Doctors</p></NavLink>
-                        <NavLink to='/about' onClick={()=>setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>About</p></NavLink>
-                        <NavLink to='/contact' onClick={()=>setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>Contact</p></NavLink>
+                        <NavLink to='/' onClick={() => setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
+                        <NavLink to='/doctors' onClick={() => setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>All Doctors</p></NavLink>
+                        <NavLink to='/about' onClick={() => setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>About</p></NavLink>
+                        <NavLink to='/contact' onClick={() => setShowMenu(false)}><p className='px-4 py-2 rounded inline-block'>Contact</p></NavLink>
                     </ul>
                 </div>
             </div>
