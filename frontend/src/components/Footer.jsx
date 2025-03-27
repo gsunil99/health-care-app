cafb2558fa5b43f676cb3f62005433a8bcdfff92
Sunil G <gsunil99910@gmail.com>
2025-03-27 23:11:17 +0530

    logo updated for footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
                 {/* left side */}
                 <div>
-                    <img className='mb-5 w-40' src={assets.logo}></img>
+                    <img className='mb-5 w-6' src={assets.logo}></img>
                     <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 {/* center */}
@@ -24,7 +24,7 @@ const Footer = () => {
                 <div>
                     <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                     <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>+1-212-456-7890</li>
+                        <li>+91 8277434979</li>
                         <li>gsunil99910@gmail.com</li>
                     </ul>
                 </div>
